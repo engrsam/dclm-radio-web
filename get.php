a11430a8ca89fa1a441de763059bd7ecfbc97317
engrsam <engrsam@users.noreply.github.com>
2021-03-17 23:10:00 +0100

    Set ignore files
    Yes, very important.

--- get.php
+++ get.php
@@ -27,60 +27,80 @@ https://www.techiediaries.com/javascript/document-ready-vs-window-onload-vs-wind
 
 <body>
 
-<div> <?php var_dump(openssl_get_cert_locations()); ?> </div>
-
-<div> <?php
-require __DIR__ . '/vendor/autoload.php';
-$api = \AzuraCast\Api\Client::create(
-	'<IP-Address | Host>',
-	'<API Key>'
-);
-
-$nowPlaying = $api->nowPlaying();
-echo $nowPlaying->getCurrentSong()->getSong()->getTitle();
-
-?> </div>
 
 
-
-
-
-
-
-
+<h1>Authors</h1>
+<ul id="authors"></ul>
+<ul id="listeners"></ul>
 
 </body>
 
 <script>
-window.addEventListener('load',
-function radioInfo(){
-// code to execute
-fetch('https://stat1.dclm.org/api/nowplaying')
-.then((res) => { return res.json() })
-.then((data) => {
-    let result = `<h2> DCLM Radio Info</h2>`;
-    data.forEach((nowplaying) => {
-        //const { id, name, email, address: { city, street } } = nowplaying
-        const { station: { id, name, listen_url }, listeners: { total }, now_playing: { elapsed, song: { title, art } } } = nowplaying;
-        if (id == 1){
-        result +=
-            `<div>
-                <h5> Station ID: ${id} </h5>
-                    <ul class="w3-ul">
-                        <li> Station Name : ${name}</li>
-                        <li> Station URL : ${listen_url} </li>
-                        <li> Station Song Title : ${title} </li>
-                        <li> Station Listeners : ${total} </li>
-                    </ul>
-            </div>`;
-            document.getElementById('art').src = `${art}`;
-            document.getElementById('result').innerHTML = result;
-            }
-            });
+function createNode(element) {
+    return document.createElement(element);
+}
 
-        })
+function append(parent, el) {
+  return parent.appendChild(el);
+}
 
+const ul = document.getElementById('authors');
+const url = 'https://stat1.dclm.org/api/nowplaying/1';
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+  let authors = data.results;
+  return authors.map(function(author) {
+    let li = createNode('li');
+    let img = createNode('img');
+    let span = createNode('span');
+    img.src = author.picture.medium;
+    span.innerHTML = `${author.name.first} ${author.name.last}`;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  })
 })
+.catch(function(error) {
+  console.log(error);
+});
+
+</script>
+
+
+
+
+<script>
+//window.addEventListener('load',
+//function radioInfo(){
+// code to execute
+//fetch('https://stat1.dclm.org/api/nowplaying/1')
+//.then((res) => { return res.json() })
+//.then((data) => {
+//    let result = `<h2> DCLM Radio Info</h2>`;
+//    data.forEach((nowplaying) => {
+        //const { id, name, email, address: { city, street } } = nowplaying
+//        const { station: { id, name, listen_url }, listeners: { total }, now_playing: { elapsed, song: { title, art } } } = nowplaying;
+        //if (id == 1){
+//        result +=
+//            `<div>
+//                <h5> Station ID: ${id} </h5>
+//                    <ul class="w3-ul">
+//                        <li> Station Name : ${name}</li>
+//                        <li> Station URL : ${listen_url} </li>
+//                        <li> Station Song Title : ${title} </li>
+//                        <li> Station Listeners : ${total} </li>
+//                    </ul>
+//            </div>`;
+//            document.getElementById('art').src = `${art}`;
+//            document.getElementById('result').innerHTML = result;
+          
+//            });
+
+//        })
+
+//})
 </script>
 
 <script>
